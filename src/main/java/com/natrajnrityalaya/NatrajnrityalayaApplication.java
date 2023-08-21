37cca6a7b96463147935d643c3408d4a690ab948
deepak.dhongade <deepak.dhongade@fyntune.com>
2023-08-21 13:42:11 +0530

    First commit

---
+++ src/main/java/com/natrajnrityalaya/NatrajnrityalayaApplication.java
@@ -0,0 +1,13 @@
+package com.natrajnrityalaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatrajnrityalayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatrajnrityalayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natrajnrityalaya/NatrajnrityalayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natrajnrityalaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatrajnrityalayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
